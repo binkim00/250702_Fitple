35190be9939997697737967f9872cf9d99514077
binkim00 <k0b0301@naver.com>
2025-09-04 16:12:41 +0900

    카카오 API 수정

--- Fitple/src/main/java/com/fitple/fitple/infra/controller/InfraController.java
+++ Fitple/src/main/java/com/fitple/fitple/infra/controller/InfraController.java
@@ -1,15 +1,19 @@
 package com.fitple.fitple.infra.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class InfraController {
 
-    @GetMapping("/infra")
-    public String test(){
-        return "infra/infra";
-    }
+    @Value("${kakao.js.key}")
+    private String kakaoJsKey;
 
+    @GetMapping("/infra")
+    public String infra(Model model) {
+        model.addAttribute("kakaoKey", kakaoJsKey);
+        return "infra"; // templates/infra.html
+    }
 }

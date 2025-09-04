d7a91053c3fa1c0665071d5ea53b91905543e577
binkim00 <k0b0301@naver.com>
2025-09-04 16:19:36 +0900

    공통 컨트롤러 수정

--- Fitple/src/main/java/com/fitple/fitple/housing/controller/HousingViewController.java
+++ Fitple/src/main/java/com/fitple/fitple/housing/controller/HousingViewController.java
@@ -1,14 +1,21 @@
 package com.fitple.fitple.housing.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HousingViewController {
 
+    @Value("${kakao.js.key}")
+    private String kakaoJsKey;
+
     @GetMapping("/housing")
-    public String housingMapPage() {
-        // templates/housing/housing_map_test.html 파일을 찾아 보여줌
+    public String housingMapPage(Model model) {
+        // 카카오 JS 키를 뷰에 전달
+        model.addAttribute("kakaoKey", kakaoJsKey);
+        // templates/housing/housing_map_test.html 렌더링
         return "housing/housing_map_test";
     }
 }
